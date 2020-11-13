d2427bcf12d386aaf1e940414254c8de96d8cf66
Jose Enrique Chacin <joc13th@gmail.com>
2020-11-13 11:05:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/login"
+            render={(props) => {
+              return <h1>Login</h1>;
+            }}
+          />
+          <Route
+            path="/"
+            render={(props) => {
+              return <h1>Root</h1>;
+            }}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
