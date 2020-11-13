d8f9a2860fd312c5c935fda8014fba4beebb7151
Jose <joc13th@gmail.com>
2020-11-13 11:48:22 -0300

    creating web socket connections

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import auth from "./store/reducers/authReducer";
+import * as ChatActions from "./store/actions/chatActions";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route
-            path="/login"
-            render={(props) => {
-              return <h1>Login</h1>;
-            }}
-          />
-          <Route
-            path="/"
-            render={(props) => {
-              return <h1>Root</h1>;
-            }}
-          />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.props.setupSocket();
+  }
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/login"
+              render={(props) => {
+                return <h1>Login</h1>;
+              }}
+            />
+            <Route
+              path="/"
+              render={(props) => {
+                return <h1>Root</h1>;
+              }}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => ({
   ...state.auth,
+  ...state.chat,
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  setupSocket: () => {
+    dispatch(ChatActions.setupSocket());
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
