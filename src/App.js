2a6acf3f705c8c5d26a1403769f2e543bc639c0d
Jose <joc13th@gmail.com>
2020-11-19 18:38:07 -0300

    Styling the messenger

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import * as ChatActions from "./store/actions/chatActions";
 import * as AuthActions from "./store/actions/authActions";
+import Messenger from "./components/pages/Messenger";
 import Auth from "./components/pages/Auth";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/swag.css";
@@ -43,12 +44,22 @@ class App extends React.Component {
               }}
             />
             <Route
+              path="/:threadId"
+              render={(props) => {
+                if (!this.props.token) {
+                  return <Redirect to="/login" />;
+                } else {
+                  return <Messenger />;
+                }
+              }}
+            />
+            <Route
               path="/"
               render={(props) => {
                 if (!this.props.token) {
                   return <Redirect to="/login" />;
                 } else {
-                  return <h1>Root</h1>;
+                  return <Messenger />;
                 }
               }}
             />
---
+++ src/components/partials/ChatInput.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Link } from "react-router-dom";
+
+class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "",
+    };
+  }
+  render() {
+    return (
+      <div className="main-view">
+        <input
+          type="text"
+          placeholder="Write your message"
+          className="form-control"
+          value={this.state.content}
+          onChange={(e) => this.setState({ content: e.target.value })}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  ...state.auth,
+  ...state.chat,
+});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(ChatInput));
